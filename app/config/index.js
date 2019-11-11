b6f658ecf1eb6a3f6fda80c047f31c9bea106e6a
Julio Mixco <jmixco@applaudostudios.com>
2019-11-10 20:57:00 -0600

    add google configs

--- app/config/index.js
+++ app/config/index.js
@@ -1,14 +1,20 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-    //offer production stage enviroment variables  
+  //offer production stage enviroment variables
 
-    module.exports = {
-        host: process.env.host || '',
-        dbURI: process.env.dbURI,
-        sessionSecret: process.env.sessionSecret,
-
-    }
+  module.exports = {
+    host: process.env.host || '',
+    dbURI: process.env.dbURI,
+    sessionSecret: process.env.sessionSecret,
+    google: {
+      clientID:
+        '916830606516-ne6letfmkphkm3h4k849n149dbsuabm0.apps.googleusercontent.com',
+      clientSecret: 'WS9Umx9Bo-0fzy3wVykTPduc',
+      callbackURL: '//localhost:3000/api/auth/google/callback',
+      profileFields: ['id', 'displayName', 'photos', 'name', 'emails'],
+    },
+  };
 } else {
-    module.exports = require('./development.json');
+  module.exports = require('./development.json');
 }
